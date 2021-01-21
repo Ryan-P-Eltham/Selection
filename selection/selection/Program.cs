f8021370067c2bdd03960b989a23003e5f7b94b8
School <no>
2021-01-21 11:18:38 +0000

    I don't know what Im doing

--- selection/selection/Program.cs
+++ selection/selection/Program.cs
@@ -4,11 +4,51 @@ namespace selection
 {
     class Program
     {
-        static void Main(string[] args)               da
+        static void Main(string[] args)
         {
-            PassFail();
+            string a1 = Console.ReadLine();
+            int select = Convert.ToInt32(a1);
+            switch(select)
+            {
+                case 1:
+                    string oof = Console.ReadLine();
+                    int minorfaults = Convert.ToInt32(oof);
+                    PassFail(minorfaults);
+                    // I forgot how subroutines work and this works for some reason?
+                    break;
+                case 2:
+                    //forgot how arrays work so doing the states of water one
+                    string a2 = Console.ReadLine();
+                    double idkwhatthisis = Convert.ToDouble(a2);
+                    StatesOfWater(idkwhatthisis);
+                    break;
+            }
         }
-        static string PassFail(int minorfaults)
-
+        static void PassFail(int minorfaults)
+        {
+            if(minorfaults >= 16)
+            {
+                Console.WriteLine("Fail");
+            }
+            else
+            {
+                Console.WriteLine("Pass");
+                Console.ReadLine();
+            }
+        }
+        static void StatesOfWater(double idkwhatthisis)
+        {
+            if(idkwhatthisis >= 100)
+            {
+                Console.WriteLine("Gaseous");
+            }
+            if (idkwhatthisis >= 1)
+            {
+                if(idkwhatthisis <= 99)
+                {
+                    Console.WriteLine("Liquid");
+                }
+            }
+        }
     }
 }
